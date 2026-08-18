9a17ce52d76bf18425a507a237387d331edce111
scott-e-higgins <scott.e.higgins@me.com>
2026-08-18 18:30:08 -0400

    Align Supabase migration history

--- supabase/migrations/20260819000000_allow_unknown_acquisition_dates.sql
+++
@@ -1,4 +0,0 @@
--- Historical imports may not include an acquisition date. Preserve that uncertainty
--- instead of inventing a date; newly recorded purchases still require one in the UI.
-alter table public.purchases
-  alter column acquisition_date drop not null;
